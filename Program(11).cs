f9fd5d923d21c685c5ba6001d4ed95a8d5cacbad
Artemmal <45432448+Artemmal@users.noreply.github.com>
2019-11-19 15:28:13 +0200

    Add files via upload

---
+++ Program(11).cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HW23._5
+{
+    abstract class Youtube
+    {
+        public abstract void VideoCast();
+    }
+    class Video : Youtube
+    {
+        string name = "Movie";
+        int duration = 56;
+        public override void VideoCast()
+        {
+            Console.WriteLine($"Casting video {name} during {duration} minutes");
+        }
+    }
+    class OfflineStorage : Youtube
+    {
+        Video video;
+        public override void VideoCast()
+        {
+            if(video == null)
+            {
+                Console.WriteLine("Adding video to the offline storage");
+                video = new Video();
+            }
+            video.VideoCast();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Youtube youtubeVideo = new OfflineStorage();
+            youtubeVideo.VideoCast();
+            youtubeVideo = new Video();
+            youtubeVideo.VideoCast();
+        }
+    }
+}
